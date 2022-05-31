946ec2de14ba77a9099e4ae8650ccb6bbc17a052
Aziz3131 <azizjon973650309@gamil.com>
2022-05-31 11:36:42 +0500

    first bot

---
+++ main.py
@@ -0,0 +1,19 @@
+from telegram.ext import Updater,CallbackContext,CommandHandler
+from telegram.update import Update
+import settings
+
+updater = Updater(token = settings.TELEGRAM_TOKEN)
+
+
+
+def start(update:Update,context:CallbackContext):
+    update.message.reply_text("Salom")
+    context.bot.send_message(chat_id=update.message.chat_id, text="Assalom")
+
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start',start))
+
+
+
+updater.start_polling()
+updater.idle()
